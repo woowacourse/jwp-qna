68cebc60f963609f25b625aceec350fa3652d774
Jeong Jinwoo <jwjeong96@gmail.com>
2022-07-08 03:04:44 +0900

    fix(QnaService): remove AnswerRepository
    - fix broken contextLoading test

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -22,12 +22,10 @@ public class QnaService {
     private static final Logger log = LoggerFactory.getLogger(QnaService.class);
 
     private final QuestionRepository questionRepository;
-    private final AnswerRepository answerRepository;
     private final DeleteHistoryService deleteHistoryService;
 
-    public QnaService(QuestionRepository questionRepository, AnswerRepository answerRepository, DeleteHistoryService deleteHistoryService) {
+    public QnaService(QuestionRepository questionRepository, DeleteHistoryService deleteHistoryService) {
         this.questionRepository = questionRepository;
-        this.answerRepository = answerRepository;
         this.deleteHistoryService = deleteHistoryService;
     }
 
@@ -41,9 +39,7 @@ public class QnaService {
     public void deleteQuestion(User loginUser, Long questionId) throws CannotDeleteException {
         Question question = findQuestionById(questionId);
         validateQuestionOwner(loginUser, question);
-
-        List<Answer> answers = answerRepository.findByQuestionIdAndDeletedFalse(questionId);
-        validateQuestionContainsOnlyMyAnswers(loginUser, answers);
+        validateQuestionContainsOnlyMyAnswers(loginUser, question);
 
         deleteHistoryService.saveAll(question.delete());
         log.info("해당 질문이 삭제되었습니다.");
@@ -55,8 +51,8 @@ public class QnaService {
         }
     }
 
-    private void validateQuestionContainsOnlyMyAnswers(User loginUser, List<Answer> answers) {
-        for (Answer answer : answers) {
+    private void validateQuestionContainsOnlyMyAnswers(User loginUser, Question question) {
+        for (Answer answer : question.getAnswers()) {
             if (!answer.isOwner(loginUser)) {
                 throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
             }
