cac594aa1aa1a564c7b2fad44626ade4e272b0d6
yoon <duddbs774@gmail.com>
2022-07-08 14:39:55 +0900

    test: 테스트 코드 추가

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -15,13 +15,33 @@ public class QuestionRepositoryTest {
     @Test
     void saveQuestion() {
         // given
-        Question question = new Question("제목","내용");
+        Question question = new Question("제목", "내용");
 
         // when
         Question actual = questionRepository.save(question);
 
         // then
         assertThat(actual.getId()).isNotNull();
+        assertThat(question).isSameAs(actual);
+    }
+
+    @Test
+    void findByDeletedFalse() {
+        // given
+        Question question = questionRepository.save(new Question("제목", "내용"));
+        Question actual = questionRepository.findByDeletedFalse().get(0);
+
+        // when, then
+        assertThat(question).isSameAs(actual);
+    }
+
+    @Test
+    void findByIdAndDeletedFalse() {
+        // given
+        Question question = questionRepository.save(new Question("제목", "내용"));
+        Question actual = questionRepository.findByIdAndDeletedFalse(question.getId()).get();
+
+        // when, then
         assertThat(question).isSameAs(actual);
     }
 }
--- src/test/java/qna/domain/UserRepositoryTest.java
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -24,4 +24,16 @@ public class UserRepositoryTest {
         assertThat(actual.getId()).isNotNull();
         assertThat(actual).isSameAs(user);
     }
+
+    @Test
+    void findByUserId() {
+        // given
+        User user = userRepository.save(new User("user", "1234", "이름", "이메일@gmail.com"));
+
+        // when
+        User actual = userRepository.findByUserId(user.getUserId()).get();
+
+        // then
+        assertThat(user).isSameAs(actual);
+    }
 }
