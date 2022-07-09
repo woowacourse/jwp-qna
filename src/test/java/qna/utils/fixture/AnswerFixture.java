8e86574d4259f9d9273386b5ac0f12e259e7a5c9
injoon2019 <ison@sfu.ca>
2022-07-09 17:45:36 +0900

    feat: Answer 도메인 구현 및 테스트

---
+++ src/test/java/qna/utils/fixture/AnswerFixture.java
@@ -0,0 +1,8 @@
+package qna.utils.fixture;
+
+import qna.domain.Answer;
+
+public class AnswerFixture {
+    public static final Answer A1 = new Answer(UserFixture.JAVAJIGI, QuestionFixture.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(UserFixture.SANJIGI, QuestionFixture.Q1, "Answers Contents2");
+}
