549397bc62951196818c06968f0ad7802f3e77e8
윤주리 <78091011+jurlring@users.noreply.github.com>
2022-07-10 22:28:35 +0900

    [주디] 1단계: 엔티티 매핑 (#13)
    * feat: user entity 구현
    
    * test: user repository test 구현
    
    * feat: question entity 구현
    
    * test: question repository test 구현
    
    * feat: answer entity 구현
    
    * test: answer repository test 구현
    
    * feat: deleteHistory entity 구현
    
    * style: code format

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,16 +1,45 @@
 package qna.domain;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
-import java.util.Objects;
-
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long writerId;
-    private Long questionId;
+
+    @Lob
     private String contents;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
     private boolean deleted = false;
+
+    private Long questionId;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private Long writerId;
+
+    protected Answer() {
+
+    }
 
     public Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
