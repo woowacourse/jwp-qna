fd68babf54b5c601d01b475d15c2d8e0adfc7822
yh20studio(tiki) <yh20studio@gmail.com>
2022-07-10 17:30:30 +0900

    fix: DataJpaTest시 내장 h2 환경을 사용하지 않고, application.properties 에서 설정한 dataSource를 사용하도록 변경

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -10,11 +10,14 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import qna.config.JpaConfig;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @Import(JpaConfig.class)
 class QuestionRepositoryTest {
 
