749c5a698f93b9d45653a933d83873e7016011fd
Wishoon <heonga2@gmail.com>
2022-07-10 16:17:11 +0900

    refactor: 페키지 재배치 및 사용되지 않는 클래스 제거
    - fixture 정리 및 제거
    - fixture 패키지 구조 변경

--- src/test/java/qna/domain/AnswerFixture.java
+++
@@ -1,9 +0,0 @@
-package qna.domain;
-
-import qna.fixture.QuestionFixture;
-
-public class AnswerFixture {
-
-    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionFixture.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionFixture.Q1, "Answers Contents2");
-}
