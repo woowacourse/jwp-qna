1067704a3a5631db540f3576b4847a540174e05c
Alexander <63737500+yxxnghwan@users.noreply.github.com>
2022-07-10 21:55:38 +0900

    [알렉스] 1단계: 엔티티 매핑 (#6)
    * feat: 1단계 - 엔티티 매핑
    
    * feat: 동일성 검증 추가

---
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -0,0 +1,49 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class AnswerRepositoryTest {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    private final User user
+            = new User("yxxnghwan", "password", "알렉스", "younghwan960@gmail.com");
+    private final Question question = new Question("JPA 어케 잘하죠", "제곧내");
+
+    @BeforeEach
+    void setUp() {
+        userRepository.save(user);
+        question.writeBy(user);
+        questionRepository.save(question);
+    }
+
+    @Test
+    @DisplayName("answer를 저장한다.")
+    void save() {
+        final Answer answer = new Answer(user, question, "제이슨 보고 배우면 됩니다!");
+
+        final Answer saved = answerRepository.save(answer);
+
+        assertAll(
+                () -> assertThat(saved).extracting("writerId", "questionId", "contents")
+                        .containsExactly(user.getId(), question.getId(), answer.getContents()),
+                () -> assertThat(saved).isSameAs(answer)
+        );
+    }
+}
---
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("유저를 저장한다.")
+    void save() {
+        final String userId = "yxxnghwan";
+        final String password = "password";
+        final String name = "알렉스";
+        final String email = "younghwan960@gmail.com";
+        final User user = new User(userId, password, name, email);
+
+        final User saved = userRepository.save(user);
+
+        assertAll(
+                () -> assertThat(saved).extracting("userId", "password", "name", "email")
+                        .containsExactly(userId, password, name, email),
+                () -> assertThat(saved).isSameAs(user)
+        );
+    }
+}
