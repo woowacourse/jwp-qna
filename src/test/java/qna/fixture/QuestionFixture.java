1265cbada452ffef362223b738297154eefbff47
Wishoon <heonga2@gmail.com>
2022-07-10 15:18:31 +0900

    feat: Question 도메인 구현
    - Question 도메인 구현
    - Question 도메인 테스트 구현
    - User, Question 도메인 Fixture 작성

---
+++ src/test/java/qna/fixture/QuestionFixture.java
@@ -0,0 +1,9 @@
+package qna.fixture;
+
+import qna.domain.Question;
+
+public class QuestionFixture {
+
+    public static final Question Q1 = new Question("title1", "contents1").writeBy(UserFixture.JAVAJIGI);
+    public static final Question Q2 = new Question("title2", "contents2").writeBy(UserFixture.SANJIGI);
+}
