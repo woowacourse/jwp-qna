0768e003f8d437298793f3c4c72414f332e8cbe3
Wishoon <heonga2@gmail.com>
2022-07-10 15:48:23 +0900

    feat: Answer 도메인 구현
    - Answer 도메인 구현
    - Answer 도메인 테스트 구현
    - Answer 도메인 Fixture 작성

---
+++ src/test/java/qna/domain/AnswerFixture.java
@@ -0,0 +1,9 @@
+package qna.domain;
+
+import qna.fixture.QuestionFixture;
+
+public class AnswerFixture {
+
+    public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionFixture.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionFixture.Q1, "Answers Contents2");
+}
---
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -0,0 +1,49 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class AnswerRepositoryTest {
+
+    @Autowired
+    private AnswerRepository answers;
+
+    @Test
+    @DisplayName("답변을 저장한다.")
+    void save() {
+        Answer expect = AnswerFixture.A1;
+        Answer actual = answers.save(expect);
+        assertThat(expect).usingRecursiveComparison()
+            .ignoringFields("id")
+            .isEqualTo(actual);
+    }
+
+    @Test
+    @DisplayName("식별자가 삭제되지 않은 조건으로 조회할시 정삭적으로 조회된다.")
+    void findByIdAndDeletedFalse_not_deleted() {
+        Answer expect = AnswerFixture.A1;
+        Answer saveQuestion = answers.save(expect);
+
+        Optional<Answer> found = answers.findByIdAndDeletedFalse(saveQuestion.getId());
+
+        assertThat(found).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("식별자와 삭제된 조건으로 조회할시 정상적으로 조회되지 않는다")
+    void findByIdAndDeletedFalse_not_true() {
+        Answer expect = AnswerFixture.A1;
+        Answer saveQuestion = answers.save(expect);
+
+        saveQuestion.setDeleted(true);
+        Optional<Answer> found = answers.findByIdAndDeletedFalse(saveQuestion.getId());
+
+        assertThat(found).isEmpty();
+    }
+}
