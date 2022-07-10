ee2cf8a7a6ae1aa4cdaf596fe15e7f8bd59a1870
yh20studio(tiki) <yh20studio@gmail.com>
2022-07-10 17:34:53 +0900

    test: RepositoryTest 어노테이션 활용하여 테스트할 수 있도록 변경

--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -9,15 +9,9 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import qna.config.JpaConfig;
+import qna.support.RepositoryTest;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-@Import(JpaConfig.class)
+@RepositoryTest
 class AnswerRepositoryTest {
 
     @Autowired
--- src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
+++ src/test/java/qna/domain/DeleteHistoryRepositoryTest.java
@@ -6,15 +6,9 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import qna.config.JpaConfig;
+import qna.support.RepositoryTest;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-@Import(JpaConfig.class)
+@RepositoryTest
 class DeleteHistoryRepositoryTest {
 
     @Autowired
