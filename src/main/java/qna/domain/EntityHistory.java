246c4a9ffe06ad19d597e3e08ca4fa33b6d72f53
Jeong Jinwoo <jwjeong96@gmail.com>
2022-07-11 19:17:31 +0900

    feat(EntityHistory): enable auditing
    - auto-update createdAt and updatedAt

--- src/main/java/qna/domain/EntityHistory.java
+++ src/main/java/qna/domain/EntityHistory.java
@@ -2,13 +2,28 @@ package qna.domain;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class EntityHistory {
 
+    @CreatedDate
     @Column(nullable = false)
-    protected LocalDateTime createdAt = LocalDateTime.now();
+    protected LocalDateTime createdAt;
 
+    @LastModifiedDate
     protected LocalDateTime updatedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 }
