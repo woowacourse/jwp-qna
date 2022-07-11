496ef0d80e9616ed073ae62eb49a1b7bb2d01ca5
injoon2019 <ison@sfu.ca>
2022-07-11 21:31:16 +0900

    feat: Auditing 적용

--- src/main/java/qna/domain/BaseEntity.java
+++ src/main/java/qna/domain/BaseEntity.java
@@ -2,13 +2,20 @@ package qna.domain;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Column(nullable = false)
+    @CreatedDate
     protected LocalDateTime createdAt = LocalDateTime.now();
+    @LastModifiedDate
     protected LocalDateTime updatedAt = LocalDateTime.now();
 
     protected BaseEntity() {
