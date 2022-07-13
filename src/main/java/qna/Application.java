594faedb06f724d2a16cb8fd2ac2f144062b7ccb
jurlring <78091011+jurlring@users.noreply.github.com>
2022-07-14 00:03:13 +0900

    refactor: 슬라이스 테스트를 위한 설정파일 분리

--- src/main/java/qna/Application.java
+++ src/main/java/qna/Application.java
@@ -2,9 +2,9 @@ package qna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Import;
 
-@EnableJpaAuditing
+@Import(JpaAuditingConfig.class)
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
