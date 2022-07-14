1e077faa4c79ae1b491cd5f8820c2536d1105463
김찬 <gmelddl514@gmail.com>
2022-07-14 22:39:42 +0900

    test: 깨지는 테스트 수정

--- src/test/java/qna/domain/QuestionRepositoryTest.java
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -1,8 +1,7 @@
 package qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static qna.domain.QuestionTest.Q1;
-import static qna.domain.QuestionTest.Q2;
+import static qna.domain.UserTest.JAVAJIGI;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,10 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import qna.config.JpaAuditingConfig;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Import(JpaAuditingConfig.class)
 class QuestionRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private QuestionRepository questionRepository;
@@ -24,6 +29,9 @@ class QuestionRepositoryTest {
     @Test
     @DisplayName("deleted가 false인 질문들을 조회한다.")
     void findByDeletedFalse() {
+        User user = userRepository.save(JAVAJIGI);
+        Question Q1 = new Question("title1", "contents1").writeBy(user);
+        Question Q2 = new Question("title2", "contents2").writeBy(user);
         List<Question> questions = questionRepository.saveAll(Arrays.asList(Q1, Q2));
 
         List<Question> findQuestions = questionRepository.findByDeletedFalse();
@@ -34,6 +42,8 @@ class QuestionRepositoryTest {
     @Test
     @DisplayName("deleted가 false인 질문을 Id로 조회한다.")
     void findByIdAndDeletedFalse() {
+        User user = userRepository.save(JAVAJIGI);
+        Question Q1 = new Question("title1", "contents1").writeBy(user);
         Question question = questionRepository.save(Q1);
 
         Optional<Question> findQuestion = questionRepository.findByIdAndDeletedFalse(question.getId());
--- src/test/java/qna/domain/UserRepositoryTest.java
+++ src/test/java/qna/domain/UserRepositoryTest.java
@@ -10,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import qna.config.JpaAuditingConfig;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Import(JpaAuditingConfig.class)
 class UserRepositoryTest {
 
     @Autowired
