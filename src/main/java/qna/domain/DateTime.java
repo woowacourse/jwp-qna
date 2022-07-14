d9b557aece5aa5c8fa2824f7b2f904672302e880
jurlring <78091011+jurlring@users.noreply.github.com>
2022-07-14 09:57:20 +0900

    refactor: 제약조건 추가

--- src/main/java/qna/domain/DateTime.java
+++ src/main/java/qna/domain/DateTime.java
@@ -1,6 +1,7 @@
 package qna.domain;
 
 import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -9,6 +10,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 public abstract class DateTime {
 
     @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
