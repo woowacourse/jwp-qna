b84bd44e8e645dc70e223f8767f4233cfe9aec7a
yukong <kyb062666@gmail.com>
2022-07-17 20:15:47 +0900

    feat: Auditing 기능 이용하여 생성 및 수정 이력 자동 업데이트 추가

--- src/main/java/qna/domain/Time.java
+++ src/main/java/qna/domain/Time.java
@@ -3,15 +3,23 @@ package qna.domain;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Time {
 
     @Column(name = "created_at", nullable = false)
-    private LocalDateTime createDate = LocalDateTime.now();
+    @CreatedDate
+    private LocalDateTime createDate;
 
     @Column(name = "updated_at")
+    @LastModifiedDate
     private LocalDateTime updateDate;
 
     protected void updateDate() {
