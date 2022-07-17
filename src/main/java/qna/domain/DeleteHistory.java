9d2713b82ad348c4ae901b82ca53c0d0831995ba
Wishoon <heonga2@gmail.com>
2022-07-17 19:08:26 +0900

    refactor: 코드 컨벤션 적용

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -11,7 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class DeleteHistory extends BaseEntity{
+public class DeleteHistory extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
