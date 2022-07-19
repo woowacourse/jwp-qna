c6dc5f70706b4036572cd79acb84cc27e2632ac4
kimchan123 <55445564+kimchan123@users.noreply.github.com>
2022-07-19 10:52:14 +0900

    [찬] 2단계: 연관 관계 매핑 (#34)
    * feat: JpaAuditingConfig 분리
    
    * feat: 연관 관계 매핑 구현
    
    * test: 깨지는 테스트 수정
    
    * fix: 생성자에서 createDate 제거

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -31,16 +31,16 @@ public class User extends BaseEntity {
     protected User() {
     }
 
+    public User(String userId, String password, String name, String email) {
+        this(null, userId, password, name, email);
+    }
+
     public User(Long id, String userId, String password, String name, String email) {
         this.id = id;
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
-    }
-
-    public User(String userId, String password, String name, String email) {
-        this(null, userId, password, name, email);
     }
 
     public void update(User loginUser, User target) {
--- src/test/java/qna/domain/AnswerRepositoryTest.java
+++ src/test/java/qna/domain/AnswerRepositoryTest.java
@@ -1,7 +1,6 @@
 package qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static qna.domain.QuestionTest.Q1;
 import static qna.domain.UserTest.JAVAJIGI;
 
 import java.util.Arrays;
@@ -13,9 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import qna.config.JpaAuditingConfig;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Import(JpaAuditingConfig.class)
 class AnswerRepositoryTest {
 
     @Autowired
@@ -31,6 +33,7 @@ class AnswerRepositoryTest {
     @DisplayName("deleted가 false인 답변들을 질문 Id로 조회한다.")
     void findByQuestionIdAndDeletedFalse() {
         User user = userRepository.save(JAVAJIGI);
+        Question Q1 = new Question("title1", "contents1").writeBy(user);
         Question question = questionRepository.save(Q1);
         List<Answer> answers = answerRepository.saveAll(Arrays.asList(
                 new Answer(user, question, "content1"),
@@ -46,6 +49,7 @@ class AnswerRepositoryTest {
     @DisplayName("deleted가 false인 답변을 Id로 조회한다.")
     void findByIdAndDeletedFalse() {
         User user = userRepository.save(JAVAJIGI);
+        Question Q1 = new Question("title1", "contents1").writeBy(user);
         Question question = questionRepository.save(Q1);
         Answer answer = answerRepository.save(new Answer(user, question, "content"));
 
