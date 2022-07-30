124e9b8d5f3e460cd5629e08e2d3976d06ebaabf
Seongwon97 <osw970325@naver.com>
2022-07-30 15:44:02 +0900

    refactor: BaseEntity를 추상 클래스로 변경

--- src/main/java/qna/domain/BaseEntity.java
+++ src/main/java/qna/domain/BaseEntity.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @CreatedDate
     @Column(nullable = false)
