f4d34dcae9cd8039b4357a56e6ae436bb618598a
Jeong Jinwoo <jwjeong96@gmail.com>
2022-07-31 19:20:28 +0900

    test(Question): group tests by methods

--- src/test/java/qna/domain/question/QuestionTest.java
+++ src/test/java/qna/domain/question/QuestionTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -41,69 +43,79 @@ class QuestionTest {
         newQuestion = questions.save(new Question("title1", "contents1", user));
     }
 
-    @Test
-    void getWriterId_메서드처럼_클래스_내부에서_연관관계를_호출하려는_경우에도_SELECT문_실행하여_지연로딩_발생() {
-        List<Question> allQuestions = questions.findAll();
-        Question question = allQuestions.get(0);
+    @DisplayName("findAll 메서드 호출시, SELECT문 실행 여부 및 대상 검증")
+    @Nested
+    class FindAllTest {
 
-        Long writerId = question.getWriterId();
-        assertThat(writerId).isNotNull();
+        @Test
+        void getWriterId_메서드처럼_클래스_내부에서_연관관계를_호출하려는_경우에도_SELECT문_실행하여_지연로딩_발생() {
+            List<Question> allQuestions = questions.findAll();
+            Question question = allQuestions.get(0);
+
+            Long writerId = question.getWriterId();
+            assertThat(writerId).isNotNull();
+        }
+
+        @Test
+        void 개별_answer에서_연관된_question_접근하여_조회_및_수정_가능() {
+            answers.save(new Answer(user, newQuestion, "contents1"));
+            answers.saveAndFlush(new Answer(user, newQuestion, "contents2"));
+            entityManager.clear(); // 영속성 컨텍스틀르 비워줘야 제대로 SELECT문들 실행
+
+            Answer answer = answers.findAll().get(0);
+
+            Question question = answer.getQuestion();
+            assertThat(question).isNotNull();
+        }
+
+        @Test
+        void question에서_연관된_answers_조회_가능() {
+            answers.save(new Answer(user, newQuestion, "contents1"));
+            answers.saveAndFlush(new Answer(user, newQuestion, "contents2"));
+            entityManager.clear();
+
+            Question question = questions.findAll().get(0);
+
+            assertThat(question.getAnswers()).hasSize(2);
+        }
+
+        @Test
+        void flush를_실행하지_않아_이미_영속성_컨텍스트에_연관관계가_존재하는_경우_별도로_SELECT문을_실행하지_않으므로_로딩되지_않음() {
+            answers.save(new Answer(user, newQuestion, "contents1"));
+            answers.saveAndFlush(new Answer(user, newQuestion, "contents2"));
+
+            Question question = questions.findAll().get(0);
+
+            assertThat(question.getAnswers()).isEmpty();
+        }
     }
 
-    @Test
-    void 개별_answer에서_연관된_question_접근하여_조회_및_수정_가능() {
-        answers.save(new Answer(user, newQuestion, "contents1"));
-        answers.saveAndFlush(new Answer(user, newQuestion, "contents2"));
-        entityManager.clear(); // 영속성 컨텍스틀르 비워줘야 제대로 SELECT문들 실행
+    @DisplayName("delete 메서드 상태 변화 및 반환값 검증")
+    @Nested
+    class DeleteTest {
 
-        Answer answer = answers.findAll().get(0);
+        @Test
+        void delete_메서드는_현재_데이터를_삭제된_상태로_변경() {
+            Question question = new Question("title2", "contents2", user);
 
-        Question question = answer.getQuestion();
-        assertThat(question).isNotNull();
-    }
+            question.delete();
 
-    @Test
-    void question에서_연관된_answers_조회_가능() {
-        answers.save(new Answer(user, newQuestion, "contents1"));
-        answers.saveAndFlush(new Answer(user, newQuestion, "contents2"));
-        entityManager.clear();
+            assertThat(question.isDeleted()).isEqualTo(true);
+        }
 
-        Question question = questions.findAll().get(0);
+        @Test
+        void delete_메서드는_현재_데이터_자체와_연관된_데이터에_대한_DeleteHistory_리스트_반환() {
+            Question question = new Question("title2", "contents2", user);
+            Answer answer1 = new Answer(user, newQuestion, "contents1");
+            Answer answer2 = new Answer(user, newQuestion, "contents2");
+            question.addAnswer(answer1);
+            question.addAnswer(answer2);
 
-        assertThat(question.getAnswers()).hasSize(2);
-    }
+            List<DeleteHistory> actual = question.delete();
+            List<DeleteHistory> expected = List.of(DeleteHistory.of(answer1),
+                    DeleteHistory.of(answer2), DeleteHistory.of(question));
 
-    @Test
-    void flush를_실행하지_않아_이미_영속성_컨텍스트에_연관관계가_존재하는_경우_별도로_SELECT문을_실행하지_않으므로_로딩되지_않음() {
-        answers.save(new Answer(user, newQuestion, "contents1"));
-        answers.saveAndFlush(new Answer(user, newQuestion, "contents2"));
-
-        Question question = questions.findAll().get(0);
-
-        assertThat(question.getAnswers()).isEmpty();
-    }
-
-    @Test
-    void delete_메서드는_현재_데이터를_삭제된_상태로_변경() {
-        Question question = new Question("title2", "contents2", user);
-
-        question.delete();
-
-        assertThat(question.isDeleted()).isEqualTo(true);
-    }
-
-    @Test
-    void delete_메서드는_현재_데이터_자체와_연관된_데이터에_대한_DeleteHistory_리스트_반환() {
-        Question question = new Question("title2", "contents2", user);
-        Answer answer1 = new Answer(user, newQuestion, "contents1");
-        Answer answer2 = new Answer(user, newQuestion, "contents2");
-        question.addAnswer(answer1);
-        question.addAnswer(answer2);
-
-        List<DeleteHistory> actual = question.delete();
-        List<DeleteHistory> expected = List.of(DeleteHistory.of(answer1),
-                DeleteHistory.of(answer2), DeleteHistory.of(question));
-
-        assertThat(actual).isEqualTo(expected);
+            assertThat(actual).isEqualTo(expected);
+        }
     }
 }
