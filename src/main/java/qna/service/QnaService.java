94c5f1ece640f09ccc4d1d3f0c7fdc14af478469
Jeong Jinwoo <jwjeong96@gmail.com>
2022-07-31 19:36:28 +0900

    refactor(QnaService): reduce depth and rename private method validateOnlyAuthorAnswers

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -1,11 +1,9 @@
 package qna.service;
 
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import qna.domain.answer.Answer;
 import qna.domain.question.Question;
 import qna.domain.question.QuestionRepository;
 import qna.domain.user.User;
@@ -36,22 +34,23 @@ public class QnaService {
     public void deleteQuestion(User loginUser, Long questionId) {
         Question question = findQuestionById(questionId);
         validateQuestionMaker(loginUser, question);
-        validateOnlyAuthorAnswers(loginUser, question.getAnswers());
+        validateQuestionContainsOnlyAuthorAnswers(loginUser, question);
 
         deleteHistoryService.saveAll(question.delete());
     }
 
-    private void validateQuestionMaker(User loginUser, Question question) throws CannotDeleteException {
+    private void validateQuestionMaker(User loginUser, Question question) {
         if (!question.isOwner(loginUser)) {
             throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
         }
     }
 
-    private void validateOnlyAuthorAnswers(User loginUser, List<Answer> answers) throws CannotDeleteException {
-        for (Answer answer : answers) {
-            if (!answer.isOwner(loginUser)) {
-                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-            }
+    private void validateQuestionContainsOnlyAuthorAnswers(User loginUser, Question question) {
+        boolean containsNonAuthorAnswer = question.getAnswers()
+                .stream()
+                .anyMatch(answer -> !answer.isOwner(loginUser));
+        if (containsNonAuthorAnswer) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
     }
 }
