a9d192d907a91c53a52eec8126ef721f6ed1b0c6
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-04 16:49:30 +0900

    fix(Application): replace additional @ComponentScan with @Import

--- src/main/java/qna/Application.java
+++ src/main/java/qna/Application.java
@@ -2,10 +2,11 @@ package qna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import qna.config.DatabaseConfig;
 
 @SpringBootApplication
-@ComponentScan(basePackages ={ "qna.config" })
+@Import(DatabaseConfig.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
