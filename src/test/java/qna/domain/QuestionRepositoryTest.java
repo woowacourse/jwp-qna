2cfa0f1f66a613034843fc48af2e6d9627f431f5
Miseong Kim <miseongk@naver.com>
2023-07-05 17:24:22 +0900

    test: repository 테스트 작성

---
+++ src/test/java/qna/domain/QuestionRepositoryTest.java
@@ -0,0 +1,32 @@
+package qna.domain;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class QuestionRepositoryTest {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Test
+    void findByDeletedFalse() {
+        Question question = new Question("제목", "내용");
+        Question savedQuestion = questionRepository.save(question);
+        List<Question> questions = questionRepository.findByDeletedFalse();
+
+        assertThat(questions).isNotEmpty();
+    }
+
+    @Test
+    void findByIdAndDeletedFalse() {
+        Question question = new Question("제목", "내용");
+        Question savedQuestion = questionRepository.save(question);
+        Question findQuestion = questionRepository.findByIdAndDeletedFalse(savedQuestion.getId()).get();
+
+        assertThat(findQuestion).isSameAs(question);
+    }
+}
