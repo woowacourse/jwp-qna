b0097179e6fd4cd0af831c0861e9da523cdb6d26
JunDonghyuk <ehdgur4814@naver.com>
2023-07-05 11:31:40 +0900

    feat: DeleteHistory Entity 작성
    Co-authored-by: youngh0 <cyh6099@gmail.com>
    Co-authored-by: eunkeeee <eunkeeeee@gmail.com>
    Co-authored-by: yenawee <yaena0319@naver.com>

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -1,9 +1,6 @@
 package qna.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -13,10 +10,15 @@ public class DeleteHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private ContentType contentType;
+
     private Long contentId;
-    private Long deletedById;
+
+    @Enumerated(EnumType.STRING)
+    private ContentType contentType;
+
     private LocalDateTime createDate = LocalDateTime.now();
+
+    private Long deletedById;
 
     protected DeleteHistory() {
     }
