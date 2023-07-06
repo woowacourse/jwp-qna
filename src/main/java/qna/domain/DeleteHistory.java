fc6c7a9b7d86f75c3f91c9828e9c2b2f1213cdf3
java-saeng <gusdn85554@naver.com>
2023-07-06 19:37:34 +0900

    feat : DeleteHistory 엔티티 추가

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -2,45 +2,66 @@ package qna.domain;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
 
+@Entity
 public class DeleteHistory {
-    private Long id;
-    private ContentType contentType;
-    private Long contentId;
-    private Long deletedById;
-    private LocalDateTime createDate = LocalDateTime.now();
 
-    public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
-        this.contentType = contentType;
-        this.contentId = contentId;
-        this.deletedById = deletedById;
-        this.createDate = createDate;
-    }
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Enumerated(value = EnumType.STRING)
+  private ContentType contentType;
+  private Long contentId;
+  private Long deletedById;
+  @CreatedDate
+  private LocalDateTime createDate;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DeleteHistory that = (DeleteHistory) o;
-        return Objects.equals(id, that.id) &&
-                contentType == that.contentType &&
-                Objects.equals(contentId, that.contentId) &&
-                Objects.equals(deletedById, that.deletedById);
-    }
+  protected DeleteHistory() {
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, contentType, contentId, deletedById);
-    }
+  public DeleteHistory(ContentType contentType, Long contentId, Long deletedById,
+      LocalDateTime createDate) {
+    this.contentType = contentType;
+    this.contentId = contentId;
+    this.deletedById = deletedById;
+    this.createDate = createDate;
+  }
 
-    @Override
-    public String toString() {
-        return "DeleteHistory{" +
-                "id=" + id +
-                ", contentType=" + contentType +
-                ", contentId=" + contentId +
-                ", deletedById=" + deletedById +
-                ", createDate=" + createDate +
-                '}';
-    }
+  @Override
+  public boolean equals(Object o) {
+      if (this == o) {
+          return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+          return false;
+      }
+    DeleteHistory that = (DeleteHistory) o;
+    return Objects.equals(id, that.id) &&
+        contentType == that.contentType &&
+        Objects.equals(contentId, that.contentId) &&
+        Objects.equals(deletedById, that.deletedById);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, contentType, contentId, deletedById);
+  }
+
+  @Override
+  public String toString() {
+    return "DeleteHistory{" +
+        "id=" + id +
+        ", contentType=" + contentType +
+        ", contentId=" + contentId +
+        ", deletedById=" + deletedById +
+        ", createDate=" + createDate +
+        '}';
+  }
 }
