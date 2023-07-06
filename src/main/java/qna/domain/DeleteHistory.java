15a2ff8a1ebf381d62f60cdbfbec4aed4bb7dcc6
Doy <doyoungwork@gmail.com>
2023-07-06 09:23:52 +0900

    [1단계 - 엔티티 매핑] 도이 미션 제출합니다. (#55)
    * chore: DB, jpa 환경설정
    
    - Dialect 적용을 같이 학습하기 위해 MySQL Dialect 설정
    
    * feat: 기존 도메인 클래스 엔티티 매핑
    
    - 엔티티 간 공통 속성인 생성일, 수정일을 BaseEntity로 분리
    - 시간에 대해 자동으로 값을 넣어주는 Auditing 기능 적용
    
    * test: UserRepository 테스트 작성
    
    * test: QuestionRepository 테스트 작성
    
    * test: AnswerRepository 테스트 작성
    
    * rename: 테스트 도메인 픽스쳐 클래스명 일괄 변경
    
    - 기존 제공된 도메인 xxxTest 클래스를 픽스쳐로 사용함에 따라 이름 변경
    
    * test: 내용이 중복되는 테스트 삭제
    
    * test: DeleteHistoryRepository 테스트 작성
    
    * chore: 초기 실행을 위해 jpa ddl auto 설정 추가
    
    * style: 어노테이션 줄바꿈 통일

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -2,13 +2,28 @@ package qna.domain;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
 
+@Entity
 public class DeleteHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(value = EnumType.STRING)
     private ContentType contentType;
     private Long contentId;
     private Long deletedById;
-    private LocalDateTime createDate = LocalDateTime.now();
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    protected DeleteHistory() {
+    }
 
     public DeleteHistory(ContentType contentType, Long contentId, Long deletedById, LocalDateTime createDate) {
         this.contentType = contentType;
@@ -17,10 +32,34 @@ public class DeleteHistory {
         this.createDate = createDate;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public ContentType getContentType() {
+        return contentType;
+    }
+
+    public Long getContentId() {
+        return contentId;
+    }
+
+    public Long getDeletedById() {
+        return deletedById;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         DeleteHistory that = (DeleteHistory) o;
         return Objects.equals(id, that.id) &&
                 contentType == that.contentType &&
