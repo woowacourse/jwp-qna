5500f7d1135554002c5d659063ebe1ba038a3ced
java-saeng <gusdn85554@naver.com>
2023-07-06 19:36:58 +0900

    feat : Question 엔티티 추가

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,83 +1,108 @@
 package qna.domain;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
 public class Question {
-    private Long id;
-    private String title;
-    private String contents;
-    private Long writerId;
-    private boolean deleted = false;
 
-    public Question(String title, String contents) {
-        this(null, title, contents);
-    }
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(length = 100, nullable = false)
+  private String title;
+  @Lob
+  private String contents;
+  private Long writerId;
+  @Column(nullable = false)
+  private boolean deleted;
+  @CreatedDate
+  @Column(nullable = false)
+  private LocalDateTime createdAt;
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
 
-    public Question(Long id, String title, String contents) {
-        this.id = id;
-        this.title = title;
-        this.contents = contents;
-    }
+  protected Question() {
+  }
 
-    public Question writeBy(User writer) {
-        this.writerId = writer.getId();
-        return this;
-    }
+  public Question(String title, String contents) {
+    this(null, title, contents);
+  }
 
-    public boolean isOwner(User writer) {
-        return this.writerId.equals(writer.getId());
-    }
+  public Question(Long id, String title, String contents) {
+    this.id = id;
+    this.title = title;
+    this.contents = contents;
+  }
 
-    public void addAnswer(Answer answer) {
-        answer.toQuestion(this);
-    }
+  public Question writeBy(User writer) {
+    this.writerId = writer.getId();
+    return this;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public boolean isOwner(User writer) {
+    return this.writerId.equals(writer.getId());
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void addAnswer(Answer answer) {
+    answer.toQuestion(this);
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getContents() {
-        return contents;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public Long getWriterId() {
-        return writerId;
-    }
+  public String getContents() {
+    return contents;
+  }
 
-    public void setWriterId(Long writerId) {
-        this.writerId = writerId;
-    }
+  public void setContents(String contents) {
+    this.contents = contents;
+  }
 
-    public boolean isDeleted() {
-        return deleted;
-    }
+  public Long getWriterId() {
+    return writerId;
+  }
 
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
+  public void setWriterId(Long writerId) {
+    this.writerId = writerId;
+  }
 
-    @Override
-    public String toString() {
-        return "Question{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", contents='" + contents + '\'' +
-                ", writerId=" + writerId +
-                ", deleted=" + deleted +
-                '}';
-    }
+  public boolean isDeleted() {
+    return deleted;
+  }
+
+  public void setDeleted(boolean deleted) {
+    this.deleted = deleted;
+  }
+
+  @Override
+  public String toString() {
+    return "Question{" +
+        "id=" + id +
+        ", title='" + title + '\'' +
+        ", contents='" + contents + '\'' +
+        ", writerId=" + writerId +
+        ", deleted=" + deleted +
+        '}';
+  }
 }
