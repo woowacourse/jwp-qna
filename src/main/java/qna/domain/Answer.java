4c6a338497f22d64b6bf8d1509629a65fa7c96a5
iamjooon2 <iamjooon2@gmail.com>
2023-07-06 16:05:27 +0900

    refactor: `@Lob` 어노테이션 추가

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
@@ -16,6 +17,7 @@ public class Answer extends BaseEntity {
     private Long id;
     private Long writerId;
     private Long questionId;
+    @Lob
     private String contents;
     private boolean deleted = false;
 
--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
 public class Question extends BaseEntity {
@@ -14,6 +15,7 @@ public class Question extends BaseEntity {
     private Long id;
     @Column(nullable = false)
     private String title;
+    @Lob
     @Column(nullable = false)
     private String contents;
     private Long writerId;
