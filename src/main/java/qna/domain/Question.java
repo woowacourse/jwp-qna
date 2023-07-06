f0fda50a0d9bd6c3aad385f103cd353f2591676c
greeng00se <thegreengoose7@gmail.com>
2023-07-06 16:24:11 +0900

    feat: Question 엔티티 매핑 추가
    Co-authored-by: beer-2000 <dhkep03@gmail.com>
    Co-authored-by: Combi153 <ssrno009@gmail.com>
    Co-authored-by: Jaeyoung22 <yt930310@snu.ac.kr>

--- src/main/java/qna/domain/Question.java
+++ src/main/java/qna/domain/Question.java
@@ -1,11 +1,31 @@
 package qna.domain;
 
-public class Question {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class Question extends BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String title;
+
+    @Lob
     private String contents;
+
     private Long writerId;
+
     private boolean deleted = false;
+
+    public Question() {
+    }
 
     public Question(String title, String contents) {
         this(null, title, contents);
@@ -30,44 +50,28 @@ public class Question {
         answer.toQuestion(this);
     }
 
-    public Long getId() {
-        return id;
+    public void changeDeleted(boolean deleted) {
+        this.deleted = deleted;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Long getId() {
+        return id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContents() {
         return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
     }
 
     public Long getWriterId() {
         return writerId;
     }
 
-    public void setWriterId(Long writerId) {
-        this.writerId = writerId;
-    }
-
     public boolean isDeleted() {
         return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
     }
 
     @Override
