3620e220f15b83a9bbb67346832b22b660705df1
donghae-kim <donghae1999@gmail.com>
2023-07-06 17:58:51 +0900

    feat: Answer 추가

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -3,14 +3,24 @@ package qna.domain;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
+import javax.persistence.*;
 import java.util.Objects;
 
-public class Answer {
+
+@Entity
+public class Answer extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long writerId;
     private Long questionId;
+    @Lob
     private String contents;
+    @Column(nullable = false)
     private boolean deleted = false;
+
+    protected Answer() {
+    }
 
     public Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
@@ -80,14 +90,4 @@ public class Answer {
         this.deleted = deleted;
     }
 
-    @Override
-    public String toString() {
-        return "Answer{" +
-                "id=" + id +
-                ", writerId=" + writerId +
-                ", questionId=" + questionId +
-                ", contents='" + contents + '\'' +
-                ", deleted=" + deleted +
-                '}';
-    }
 }
--- src/test/java/qna/domain/AnswerTest.java
+++ src/test/java/qna/domain/AnswerTest.java
@@ -1,6 +1,42 @@
 package qna.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class AnswerTest {
+    @Autowired
+    private AnswerRepository answerRepository;
     public static final Answer A1 = new Answer(UserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void saveAnswer() {
+        Answer savedAnswer = answerRepository.save(A1);
+
+        assertThat(savedAnswer.getId()).isNotNull();
+    }
+
+    @Test
+    void findAnswer() {
+        Answer savedAnswer1 = answerRepository.save(A1);
+        Answer savedAnswer2 = answerRepository.save(A2);
+        List<Answer> answers = answerRepository.findAll();
+
+        assertThat(answers.size()).isEqualTo(2);
+        assertThat(answers).contains(savedAnswer1, savedAnswer2);
+    }
+
+    @Test
+    void findAnswerById() {
+        Answer savedAnswer = answerRepository.save(A1);
+        Answer findAnswer = answerRepository.findById(savedAnswer.getId()).get();
+
+        assertThat(findAnswer).isEqualTo(savedAnswer);
+    }
 }
