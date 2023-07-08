ea5025340f5193f51c8d0a201eecba4a8215c422
java-saeng <gusdn85554@naver.com>
2023-07-08 20:49:08 +0900

    feat : mock data 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,7 @@ insert into question(id, title, contents, writer_id, deleted, created_at, update
 values (2, 'title2', 'contents2', 1, true, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 insert into delete_history(id, content_type, content_id, deleted_by_id, create_date)
-values (1, 'QUESTION', 1, 2, CURRENT_TIMESTAMP());
+values (1, 'QUESTION', 1, 1, CURRENT_TIMESTAMP());
 
 insert into answer(id, writer_id, question_id, contents, deleted, created_at, updated_at)
 values (1, 1, 1, 'contents', false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
