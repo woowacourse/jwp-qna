0908d52d3ad1b4b2145a3a4112339efca8f79cb6
pilyang <30036534+pilyang@users.noreply.github.com>
2023-07-09 17:07:10 +0900

    test: UserRepsitory 테스트 작성
    Co-authored-by: the9kim <the9rla@gmail.com>
    Co-authored-by: sh111-coder <ohk9134@naver.com>

--- src/test/java/qna/fixture/UserFixture.java
+++ src/test/java/qna/fixture/UserFixture.java
@@ -10,4 +10,8 @@ public class UserFixture {
     public static User SANJIGI() {
         return new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
     }
+
+    public static User SEONGHA() {
+        return new User("seongha", "password", "name", "seongha@slipp.net");
+    }
 }
