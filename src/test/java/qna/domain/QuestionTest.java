a8faa7ee5d752927c915b2c2b96fad62701d7871
pilyang <30036534+pilyang@users.noreply.github.com>
2023-07-09 15:12:39 +0900

    test: Question 도메인 테스트 작성
    Co-authored-by: the9kim <the9rla@gmail.com>
    Co-authored-by: sh111-coder <ohk9134@naver.com>

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -1,4 +1,37 @@
 package qna.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.fixture.QuestionFixture;
+import qna.fixture.UserFixture;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class QuestionTest {
+
+    @Test
+    @DisplayName("질문한 사용자가 맞음을 확인한다.")
+    void isOwnerTrue() {
+        // given
+        final Question question = QuestionFixture.Q1();
+
+        // when
+        final boolean actual = question.isOwner(UserFixture.JAVAJIGI());
+
+        //then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("질문한 사용자가 아님을 확인한다.")
+    void isOwnerFalse() {
+        // given
+        final Question question = QuestionFixture.Q1();
+
+        // when
+        final boolean actual = question.isOwner(UserFixture.SANJIGI());
+
+        //then
+        assertThat(actual).isFalse();
+    }
 }
