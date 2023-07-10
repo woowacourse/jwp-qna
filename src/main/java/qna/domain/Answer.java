e9c80c5b820a90ee2b491ccaf2b9927c6506d258
songusika <sws981202@gmail.com>
2023-07-10 20:43:53 +0900

    feat: Answer 엔티티 구현

--- src/main/java/qna/domain/Answer.java
+++ src/main/java/qna/domain/Answer.java
@@ -1,16 +1,34 @@
 package qna.domain;
 
+import java.util.Objects;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import qna.NotFoundException;
 import qna.UnAuthorizedException;
 
-import java.util.Objects;
-
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long writerId;
-    private Long questionId;
+    private Long questionId; // Question extends Content
+    @Lob
     private String contents;
     private boolean deleted = false;
+    @Embedded
+    private TimeLog timeLog;
+
+    protected Answer() {
+    }
 
     public Answer(User writer, Question question, String contents) {
         this(null, writer, question, contents);
