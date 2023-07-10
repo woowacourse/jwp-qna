473f66edb9af2020652878062d523717a90b98ef
dooboocookie <bkh3030@gmail.com>
2023-07-10 20:26:51 +0900

    feat: User ddl 문에 맞게 수정

--- src/main/java/qna/domain/User.java
+++ src/main/java/qna/domain/User.java
@@ -1,11 +1,11 @@
 package qna.domain;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import qna.UnAuthorizedException;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
@@ -13,11 +13,21 @@ public class User {
     public static final GuestUser GUEST_USER = new GuestUser();
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 20, nullable = false, unique = true)
     private String userId;
+    @Column(length = 20, nullable = false)
     private String password;
+    @Column(length = 20, nullable = false)
     private String name;
+    @Column(length = 50)
     private String email;
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
     protected User() {
     }
