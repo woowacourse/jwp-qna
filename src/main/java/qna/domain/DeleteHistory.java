7a16ccf007acc500754a3b36251cea6374d5056d
dooboocookie <bkh3030@gmail.com>
2023-07-10 20:19:21 +0900

    feat: DeleteHistory ddl 문에 맞게 수정

--- src/main/java/qna/domain/DeleteHistory.java
+++ src/main/java/qna/domain/DeleteHistory.java
@@ -1,5 +1,7 @@
 package qna.domain;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
