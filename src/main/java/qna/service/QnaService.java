12e0ff83cc17c6098d47c1428aa631f9722d4316
greeng00se <thegreengoose7@gmail.com>
2023-07-11 16:27:23 +0900

    style: 컨벤션에 맞춰 띄어쓰기 수정
    Co-authored-by: Combi153 <ssrno009@gmail.com>
    Co-authored-by: Jaeyoung22 <yt930310@snu.ac.kr>

--- src/main/java/qna/service/QnaService.java
+++ src/main/java/qna/service/QnaService.java
@@ -58,11 +58,13 @@ public class QnaService {
         List<DeleteHistory> deleteHistories = new ArrayList<>();
         question.changeDeleted(true);
         deleteHistories.add(
-                new DeleteHistory(ContentType.QUESTION, questionId, question.getWriterId(), LocalDateTime.now()));
+                new DeleteHistory(ContentType.QUESTION, questionId, question.getWriter(), LocalDateTime.now())
+        );
         for (Answer answer : answers) {
             answer.changeDeleted(true);
             deleteHistories.add(
-                    new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriterId(), LocalDateTime.now()));
+                    new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now())
+            );
         }
         deleteHistoryService.saveAll(deleteHistories);
     }
