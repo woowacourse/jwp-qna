1f97117728469c4e39fa4f9ab3462267f25bccfa
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-07-11 13:51:17 +0900

    refactor: 변수 이름 변경

--- src/test/java/qna/service/QnaServiceTest.java
+++ src/test/java/qna/service/QnaServiceTest.java
@@ -91,13 +91,13 @@ class QnaServiceTest {
 
     @Test
     public void delete_답변_중_다른_사람이_쓴_글() throws Exception {
-        Answer answer2 = new Answer(2L, UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents1");
-        question.addAnswer(answer2);
+        Answer otherAnswer = new Answer(2L, UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents1");
+        question.addAnswer(otherAnswer);
 
         when(questionRepository.findByIdAndDeletedFalse(question.getId())).thenReturn(
             Optional.of(question));
         when(answerRepository.findByQuestionIdAndDeletedFalse(question.getId())).thenReturn(
-            Arrays.asList(answer, answer2));
+            Arrays.asList(this.answer, otherAnswer));
 
         assertThatThrownBy(() -> qnaService.deleteQuestion(UserTest.JAVAJIGI, question.getId()))
             .isInstanceOf(CannotDeleteException.class);
