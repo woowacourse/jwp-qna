19a9bea30f8dc1172e8feb711c2f764d57d23508
jeonghoon <shb03323@naver.com>
2023-07-13 08:38:27 +0900

    test: Answer 레포지터리 테스트 Fixture 적용

--- src/test/java/qna/repository/answer/AnswerRepositoryTest.java
+++ src/test/java/qna/repository/answer/AnswerRepositoryTest.java
@@ -23,10 +23,8 @@ class AnswerRepositoryTest extends RepositoryTestConfig {
     @DisplayName("삭제되지 않은 답변 중에서 질문을 식별자로 조회한다.")
     void findByQuestionIdAndDeletedFalse() {
         // given
-        User user = new User("hyena", "1234", "헤나", "example@example.com");
-        Question question = new Question("질문이 뭐에요?", "저도 모르겠어요.");
-        userRepositoryFixture.save(user);
-        questionRepositoryFixture.save(question);
+        User user = userRepositoryFixture.save("hyena", "1234", "헤나", "example@example.com");
+        Question question = questionRepositoryFixture.save("질문이 뭐에요?", "저도 모르겠어요.");
         Answer expected = new Answer(user, question, "내용내용");
         answerRepository.save(expected);
 
@@ -41,10 +39,8 @@ class AnswerRepositoryTest extends RepositoryTestConfig {
     @DisplayName("답변이 존재하지 않을 때 목록이 비어있다.")
     void findByQuestionIdAndDeletedFalse_when_answer_does_not_exist() {
         // given
-        User user = new User("hyena", "1234", "헤나", "example@example.com");
-        Question question = new Question("질문이 뭐에요?", "저도 모르겠어요.");
-        userRepositoryFixture.save(user);
-        questionRepositoryFixture.save(question);
+        User user = userRepositoryFixture.save("hyena", "1234", "헤나", "example@example.com");
+        Question question = questionRepositoryFixture.save("질문이 뭐에요?", "저도 모르겠어요.");
 
         // when
         List<Answer> actual = answerRepository.findByQuestionIdAndDeletedFalse(question.getId());
@@ -57,10 +53,8 @@ class AnswerRepositoryTest extends RepositoryTestConfig {
     @DisplayName("삭제된 답변일 때 목록이 비어있다.")
     void findByQuestionIdAndDeletedFalse_when_answer_is_deleted() {
         // given
-        User user = new User("hyena", "1234", "헤나", "example@example.com");
-        Question question = new Question("질문이 뭐에요?", "저도 모르겠어요.");
-        userRepositoryFixture.save(user);
-        questionRepositoryFixture.save(question);
+        User user = userRepositoryFixture.save("hyena", "1234", "헤나", "example@example.com");
+        Question question = questionRepositoryFixture.save("질문이 뭐에요?", "저도 모르겠어요.");
         Answer expected = new Answer(user, question, "내용내용");
         answerRepository.save(expected);
         expected.setDeleted(true);
@@ -76,10 +70,8 @@ class AnswerRepositoryTest extends RepositoryTestConfig {
     @DisplayName("답변의 식별자로 삭제되지 않은 답변을 조회한다.")
     void findByIdAndDeletedFalse() {
         // given
-        User user = new User("hyena", "1234", "헤나", "example@example.com");
-        Question question = new Question("질문이 뭐에요?", "저도 모르겠어요.");
-        userRepositoryFixture.save(user);
-        questionRepositoryFixture.save(question);
+        User user = userRepositoryFixture.save("hyena", "1234", "헤나", "example@example.com");
+        Question question = questionRepositoryFixture.save("질문이 뭐에요?", "저도 모르겠어요.");
         Answer expected = new Answer(user, question, "내용내용");
         answerRepository.save(expected);
 
@@ -94,10 +86,8 @@ class AnswerRepositoryTest extends RepositoryTestConfig {
     @DisplayName("답변의 식별자로 답변을 검색할 때, 삭제된 경우 검색되지 않는다.")
     void findByIdAndDeletedFalse_isNotPresent() {
         // given
-        User user = new User("hyena", "1234", "헤나", "example@example.com");
-        Question question = new Question("질문이 뭐에요?", "저도 모르겠어요.");
-        userRepositoryFixture.save(user);
-        questionRepositoryFixture.save(question);
+        User user = userRepositoryFixture.save("hyena", "1234", "헤나", "example@example.com");
+        Question question = questionRepositoryFixture.save("질문이 뭐에요?", "저도 모르겠어요.");
         Answer expected = new Answer(user, question, "내용내용");
         expected.setDeleted(true);
         answerRepository.save(expected);
