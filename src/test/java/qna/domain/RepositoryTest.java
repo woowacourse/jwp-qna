cf0ea4cc885ee077f5c49ce5e5fcedf3b0309add
yujamint <woawoa0513@gmail.com>
2023-07-12 13:53:18 +0900

    refactor: EnableJpaAuditing 위치 수정

--- src/test/java/qna/domain/RepositoryTest.java
+++ src/test/java/qna/domain/RepositoryTest.java
@@ -2,7 +2,10 @@ package qna.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import qna.config.JpaAuditingConfig;
 
+@Import(JpaAuditingConfig.class)
 @DataJpaTest
 public class RepositoryTest {
     @Autowired
