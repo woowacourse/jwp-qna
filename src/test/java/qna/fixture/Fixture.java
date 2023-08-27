ff8a9c98a2e354d10bc622c782486053d0971002
iamjooon2 <iamjooon2@gmail.com>
2023-08-27 20:04:10 +0900

    fix: 사용하지 않는 fixture 제거

--- src/test/java/qna/fixture/Fixture.java
+++ src/test/java/qna/fixture/Fixture.java
@@ -6,9 +6,6 @@ public class Fixture {
 
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
-    public static final Question Q1 = new Question("title1", JAVAJIGI,"contents1");
-    public static final Question Q2 = new Question("title2", SANJIGI, "contents2");
-    public static final Answer A1 = new Answer(JAVAJIGI, Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(SANJIGI, Q1, "Answers Contents2");
+    public static final Question Q1 = new Question("title1", JAVAJIGI, "contents1");
 
 }
